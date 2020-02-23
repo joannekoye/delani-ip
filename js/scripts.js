21d42c9bc695cbb70d06580eee13bdbe4bb4be55
Joan Nekoye <joannekoye@gmail.com>
2020-02-23 20:30:18 +0300

    hover effects on images

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
+  $(".imageCaption").hide();
+  $(".floatCaption").hover(function() {
+    $(this).find(".imageCaption").toggle(500);
+  })
   $(".clickDesign").click(function() {
     $(".showDesign").slideToggle();
+    $(".hideDesign").slideToggle();
   });
   $(".clickDevelopment").click(function() {
     $(".showDevelopment").slideToggle();
+    $(".hideDevelopment").slideToggle();
   });
   $(".clickProduct").click(function() {
     $(".showProduct").slideToggle();
+    $(".hideProduct").slideToggle();
   });
+  
 });
