161680411fc6abf4c7006a3698cd1839ceb6057d
Joan Nekoye <joannekoye@gmail.com>
2020-02-23 17:17:49 +0300

    write jquery

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".clickDesign").click(function() {
+    $(".showDesign").slideToggle();
+  });
+  $(".clickDevelopment").click(function() {
+    $(".showDevelopment").slideToggle();
+  });
+  $(".clickProduct").click(function() {
+    $(".showProduct").slideToggle();
+  });
+});
