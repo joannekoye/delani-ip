efce9e60c7f2d348d524268e1d42e25867d76f9c
Joan Nekoye <joannekoye@gmail.com>
2020-02-23 21:46:02 +0300

    alert messages

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-  $(".imageCaption").hide();
   $(".floatCaption").hover(function() {
-    $(this).find(".imageCaption").toggle(500);
+    $(this).find(".imageCaption").toggle(400);
   })
   $(".clickDesign").click(function() {
     $(".showDesign").slideToggle();
@@ -14,6 +13,31 @@ $(document).ready(function() {
   $(".clickProduct").click(function() {
     $(".showProduct").slideToggle();
     $(".hideProduct").slideToggle();
-  });
-  
+  });  
 });
+
+$("button").click(function(event) {
+  event.preventDefault();
+  var name=document.getElementById('name');
+  var email= document.getElementById('email').value;
+  var message=document.getElementById('message');
+
+  if(name=""){
+   console.log("Please enter your name");
+  }
+  else if(message=""){
+    console.log("Please enter your message");
+    }
+  else if(email.length<10) {
+    console.log("Please enter a valid email address");
+  }
+  else{
+    console.log("Dear " + name + ", we have received your message. Thank you for contacting us.");
+  }
+});
+
+$("button").on ('click',function() {
+  $('form').each(function(){
+    this.reset();
+  })
+})
