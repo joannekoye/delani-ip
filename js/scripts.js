09c8ca5e57017fbb199a86e8a07cc158e899863e
Joan Nekoye <joannekoye@gmail.com>
2020-02-23 22:11:01 +0300

    add fonts

--- js/scripts.js
+++ js/scripts.js
@@ -23,16 +23,16 @@ $("button").click(function(event) {
   var message=document.getElementById('message');
 
   if(name=""){
-   console.log("Please enter your name");
+   alert("Please enter your name");
   }
   else if(message=""){
-    console.log("Please enter your message");
+    alert("Please enter your message");
     }
   else if(email.length<10) {
-    console.log("Please enter a valid email address");
+    alert("Please enter a valid email address");
   }
   else{
-    console.log("Dear " + name + ", we have received your message. Thank you for contacting us.");
+    alert("Dear " + name + ", we have received your message. Thank you for contacting us.");
   }
 });
 
