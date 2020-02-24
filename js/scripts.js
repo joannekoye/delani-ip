5b443bb8e17dfa45cac11c733d2c71efe9a00c78
Joan Nekoye <joannekoye@gmail.com>
2020-02-24 08:16:43 +0300

    make changes on form

--- js/scripts.js
+++ js/scripts.js
@@ -13,31 +13,29 @@ $(document).ready(function() {
   $(".clickProduct").click(function() {
     $(".showProduct").slideToggle();
     $(".hideProduct").slideToggle();
-  });  
-});
+  }); 
 
-$("button").click(function(event) {
-  event.preventDefault();
-  var name=document.getElementById('name');
-  var email= document.getElementById('email').value;
-  var message=document.getElementById('message');
-
-  if(name=""){
-   alert("Please enter your name");
-  }
-  else if(message=""){
-    alert("Please enter your message");
+  $("#button").click(function(event) {
+    event.preventDefault();
+    var name=document.getElementById('name').value;
+    var email= document.getElementById('email').value;
+    var message=document.getElementById('message').value;
+  
+    if(name===""){
+     alert("Please fill all the fields");
     }
-  else if(email.length<10) {
-    alert("Please enter a valid email address");
-  }
-  else{
-    alert("Dear " + name + ", we have received your message. Thank you for contacting us.");
-  }
+    else if(message===""){
+      alert("Please enter your message");
+      }
+    else if(email.length<10) {
+      alert("Please enter a valid email address");
+    }
+    else{
+      alert("Dear " + name + ", we have received your message. Thank you for contacting us.");
+    }
+  }); 
 });
 
-$("button").on ('click',function() {
-  $('form').each(function(){
-    this.reset();
-  })
-})
+
+
+
